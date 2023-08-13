33cf4d76fb202c71f9e91ed995cb71d73bf48925
JakeArent <jedimasterjake@verizon.net>
2023-08-13 15:31:25 -0400

    Dialog Validator Set

---
+++ Assets/Dialog System/Additional/DialogValidatorSet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogValidatorSet : DialogValidator
+{
+    public List<DialogValidator> Validators = new List<DialogValidator>();
+    public override bool Validate()
+    {
+        foreach (DialogValidator dv in Validators)
+            if (!dv.Validate())
+                return false;
+        return true;
+    }
+}
